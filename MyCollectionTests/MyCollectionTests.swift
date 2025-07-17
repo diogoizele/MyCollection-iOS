d76453fdc64a5adc7fda80b56b4363971ebee262
diogoizele <diogogabrielizele@gmail.com>
2025-07-17 20:11:36 -0300

    Initial Commit

---
+++ MyCollectionTests/MyCollectionTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyCollectionTests.swift
+//  MyCollectionTests
+//
+//  Created by Diogo Izele on 17/07/25.
+//
+
+import Testing
+@testable import MyCollection
+
+struct MyCollectionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
